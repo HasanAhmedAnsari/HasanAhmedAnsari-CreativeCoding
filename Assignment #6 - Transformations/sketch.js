0ce27b659d3c59373a038fd34f9e1858df49d318
HasanAhmedAnsari <haa373@nyu.edu>
2016-11-07 21:09:23 -0500

    Update sketch.js

--- Assignment #6 - Transformations/sketch.js
+++ Assignment #6 - Transformations/sketch.js
@@ -1,42 +1,32 @@
-function setup() {
-  createCanvas(windowWidth,windowHeight);
+function setup(){
+  createCanvas(500,500);
 }
-
 function draw() {
+  background(125);
+  noStroke();
+  translate(width/2,height/4);
+  noFill();
+  rotate(radians(frameCount%360));
   
-  background(255);
+  colorFrom=color(155,235,180);
+  colorTo=color(100,85,140);
   
-  for(var i=0; i< width; i+=2.5){
+  colorOne=lerpColor(colorFrom,colorTo,0.1);
+  colorTwo=lerpColor(colorFrom,colorTo,0.5);
+  
+  for(var i =0; i < 10; i++){
     push();
+    rotate(TWO_PI * i/5);
+  	
+    if(i%2==1){
+    	fill(colorOne);
+    }
+    else{
+      fill(colorTwo);
+    }
     
-    colorMode(HSB, 255,100,100);
-    var colorFrom = color(255,150,140);
-    var colorTo = color(random(80),130,160);
-    var lerpAmount = map(i, 0, width, 0, 1.0);
-    var lerpedColor = lerpColor(colorFrom, colorTo, lerpAmount);
-    fill(lerpedColor);
-  
-  translate(width/2,height/2); //Translate the object to the Center
-  rotate(i * radians(TWO_PI));
-  smiley(i,0);
-  pop();
+    triangle(frameCount%(width/2),0,200,200,250,250);
+    triangle(frameCount%(width/4),250,250,300,100,150);
+    pop();
   }
-}
-
-function smiley(posX,posY) {
-  //face
-  //fill('yellow');
-  ellipse(posX,posY,40,40);
-  
-  //left eye
-  //fill('black');
-  ellipse(posX-10,posY-10,5,5);
-  
-  //right eye
-  //fill('black');
-  ellipse(posX+10,posY-10,5,5);
-  
-  //mouth
-  //fill('black');
-  ellipse(posX,posY+8,14,8);
 }
